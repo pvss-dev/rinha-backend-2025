8e9d63ab26f879f6c4b1b1f11ff600e60eb1a4f7
Paulo Vitor Soares <paulovitorsilvasoares@gmail.com>
2025-08-01 14:51:42 -0400

    refactor: register ProcessorDetailDto for reflection and update log configurations for better debugging and noise reduction

--- src/main/java/br/com/pvssdev/application/dto/ProcessorDetailDto.java
+++ src/main/java/br/com/pvssdev/application/dto/ProcessorDetailDto.java
@@ -1,7 +1,10 @@
 package br.com.pvssdev.application.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.math.BigDecimal;
 
+@RegisterForReflection
 public record ProcessorDetailDto(
         long totalRequests,
         BigDecimal totalAmount
