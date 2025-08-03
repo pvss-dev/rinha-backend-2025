857c375e0f18a21ff0f7d1905aaab78cdc1f4a84
Paulo Vitor Soares <paulovitorsilvasoares@gmail.com>
2025-08-03 16:00:53 -0400

    refactor: integrate RedisProperties into Redis configuration and customize standalone connection settings

--- src/main/java/br/com/pvss/rinhabackend2025/config/RedisConfig.java
+++ src/main/java/br/com/pvss/rinhabackend2025/config/RedisConfig.java
@@ -1,18 +1,23 @@
 package br.com.pvss.rinhabackend2025.config;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableConfigurationProperties(RedisProperties.class)
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public RedisConnectionFactory redisConnectionFactory(RedisProperties props) {
+        var cfg = new RedisStandaloneConfiguration(props.getHost(), props.getPort());
+        return new LettuceConnectionFactory(cfg);
     }
 
     @Bean
