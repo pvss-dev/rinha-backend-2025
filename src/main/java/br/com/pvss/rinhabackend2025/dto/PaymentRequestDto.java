2459655ca448bf16f78ec41025d786050c958203
Paulo Vitor Soares <paulovitorsilvasoares@gmail.com>
2025-08-05 16:32:06 -0400

    refactor: add @JsonProperty annotations to DTO fields for consistent JSON serialization

--- src/main/java/br/com/pvss/rinhabackend2025/dto/PaymentRequestDto.java
+++ src/main/java/br/com/pvss/rinhabackend2025/dto/PaymentRequestDto.java
@@ -1,12 +1,15 @@
 package br.com.pvss.rinhabackend2025.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 
 import java.math.BigDecimal;
 
 @RegisterReflectionForBinding
 public record PaymentRequestDto(
+        @JsonProperty("correlationId")
         String correlationId,
+        @JsonProperty("amount")
         BigDecimal amount
 ) {
 }
