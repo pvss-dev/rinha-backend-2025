74fd65b67511a681c0a86fbe83b44f735b2eef79
Paulo Vitor Soares <paulovitorsilvasoares@gmail.com>
2025-08-05 15:21:46 -0400

    refactor: add @RegisterReflectionForBinding for Payment DTOs to improve AOT compatibility

--- src/main/java/br/com/pvss/rinhabackend2025/dto/PaymentRequestDto.java
+++ src/main/java/br/com/pvss/rinhabackend2025/dto/PaymentRequestDto.java
@@ -1,7 +1,10 @@
 package br.com.pvss.rinhabackend2025.dto;
 
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
+
 import java.math.BigDecimal;
 
+@RegisterReflectionForBinding
 public record PaymentRequestDto(
         String correlationId,
         BigDecimal amount,
