cc938a1b97b7874561a0328aeb287bbe21796615
Paulo Vitor Soares <paulovitorsilvasoares@gmail.com>
2025-08-06 17:42:00 -0400

    refactor: add @JsonFormat to requestedAt in ProcessorPaymentRequest for consistent JSON serialization

--- src/main/java/br/com/pvss/rinhabackend2025/dto/ProcessorPaymentRequest.java
+++ src/main/java/br/com/pvss/rinhabackend2025/dto/ProcessorPaymentRequest.java
@@ -1,5 +1,7 @@
 package br.com.pvss.rinhabackend2025.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.UUID;
@@ -7,6 +9,7 @@ import java.util.UUID;
 public record ProcessorPaymentRequest(
         UUID correlationId,
         BigDecimal amount,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, timezone = "UTC")
         Instant requestedAt
 ) {
 }
