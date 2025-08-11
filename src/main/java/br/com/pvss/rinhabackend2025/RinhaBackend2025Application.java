068f48b6d4e3e59730ecc3b3ef8f65c71e1522ce
Paulo Vitor Soares <paulovitorsilvasoares@gmail.com>
2025-08-11 16:02:47 -0400

    refactor: enable scheduling annotations in Spring Boot application startup configuration

--- src/main/java/br/com/pvss/rinhabackend2025/RinhaBackend2025Application.java
+++ src/main/java/br/com/pvss/rinhabackend2025/RinhaBackend2025Application.java
@@ -2,7 +2,9 @@ package br.com.pvss.rinhabackend2025;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class RinhaBackend2025Application {
 
